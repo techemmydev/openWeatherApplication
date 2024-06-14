86de1e65eac09d6064fc254eaea887091aee7b0a
gabrirlemmydev <gabrielemmanuelmary12345@gmail.com>
2024-06-14 18:21:01 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,45 +13,56 @@ const App = () => {
   const [query, setQuery] = useState({ q: "Nigeria" });
   const [units, setUnits] = useState("metric");
   const [weather, setWeather] = useState(null);
+
   function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
+
   const getWeather = async () => {
     try {
       const message = query.q ? query.q : "current location";
-      toast.info(`fetching weather data for ${capitalize(message)}`);
+      toast.info(`Fetching weather data for ${capitalize(message)}`);
       const data = await getFormattedWeatherData({ ...query, units });
-      toast.success(`fetching weather data for${data.name},${data.country} `);
+      toast.success(`Fetched weather data for ${data.name}, ${data.country}`);
       setWeather(data);
     } catch (error) {
       console.error("Error fetching weather data:", error);
+      toast.error(`Error fetching weather data: ${error.message}`);
     }
   };
 
   useEffect(() => {
     getWeather();
   }, [query, units]);
-  const formatBAckground = () => {
+
+  const formatBackground = () => {
     if (!weather) return "from-cyan-600 to-blue-700";
     const threshold = units === "metric" ? 20 : 60;
     if (weather.temp <= threshold) return "from-cyan-600 to-blue-700";
     return "from-yellow-600 to-orange-700";
   };
+
   return (
     <div
-      className={`"mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700" ${formatBAckground()}`}
+      className={`min-h-screen w-full py-5 px-4 bg-gradient-to-br shadow-xl ${formatBackground()}`}
     >
-      <TobButttons setQuery={setQuery} />
-      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
-      {weather && (
-        <>
-          <TimeAndLocation weather={weather} />
-          <TempAndDetails weather={weather} units={units} />
-          <ForeCast title="hourly forecast" data={weather.hourly} />
-          <ForeCast title="daily forecast" data={weather.daily} />
-        </>
-      )}
-      <ToastContainer autoClose={2500} hideProgressBar={true} theme="colored" />
+      <div className="mx-auto max-w-screen-lg">
+        <TobButttons setQuery={setQuery} />
+        <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+        {weather && (
+          <>
+            <TimeAndLocation weather={weather} />
+            <TempAndDetails weather={weather} units={units} />
+            <ForeCast title="hourly forecast" data={weather.hourly} />
+            <ForeCast title="daily forecast" data={weather.daily} />
+          </>
+        )}
+        <ToastContainer
+          autoClose={2500}
+          hideProgressBar={true}
+          theme="colored"
+        />
+      </div>
     </div>
   );
 };
